7598d02df0d070e21c61161e2040271c7805bfd2
BogdanAPS <114151711+BogdanAPS@users.noreply.github.com>
2023-11-01 19:57:33 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import "./styles/main.css"
+
+import QuoteOfTheDay from "./components/main/QuoteOfTheDay";
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <QuoteOfTheDay />
+     
+    </div>
+  );
+}
+
+export default App;
